b43aab9c4d26781a1a19fa3ff8d3bdd83e1df270
Patricia Nogueira De Santana Silva <patricia.silva@fcamara.com.br>
2023-08-16 15:18:13 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findyUserByDocument(String document);
+    Optional<User> findyUserById(Long id);
+}
